40fda87d11e24826602f450b83121987c6df0737
Mateusz Kruk <krukm634@gmail.com>
2021-12-28 15:59:35 +0100

    Fixing the animations

---
+++ Assets/Window.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Window
+{
+    public string name;
+    public Canvas window;
+}
