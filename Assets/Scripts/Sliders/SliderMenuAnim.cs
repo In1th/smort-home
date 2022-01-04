59593c9924fdd5ef227dcc2bf2be4836827ca8d6
Mateusz Kruk <krukm634@gmail.com>
2022-01-04 23:10:34 +0100

    Diming screen, screen transition, DOM screen

--- Assets/Scripts/Sliders/SliderMenuAnim.cs
+++ Assets/Scripts/Sliders/SliderMenuAnim.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class SliderMenuAnim : MonoBehaviour
 {
     public GameObject PanelMenu;
+    public GameObject DimingScreen;
     public Button menuButton;
-    
 
     public void ShowHideMenu(){
     
-        if (PanelMenu != null){
+        if (PanelMenu != null && DimingScreen != null){
             Animator animator = PanelMenu.GetComponent<Animator>();
+            Animator dimingAnimator = DimingScreen.GetComponent<Animator>();
+            
             if (animator != null){
                 bool isOpen = animator.GetBool("show");
                 animator.SetBool("show",!isOpen);
+                if (dimingAnimator != null) dimingAnimator.SetBool("dim",!isOpen);
 
                 if (menuButton != null){
                     Text buttonText = menuButton.GetComponentInChildren<Text>();
